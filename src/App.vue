b19b8e90bba3b9d671a4cf8e07bc0eb361c76be7
McChen <McChen1024@163.com>
2018-11-19 20:24:49 +0800

    tabBar路由选中高亮,定义路由匹配规则,抽离页面

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,25 @@
     <mt-header fixed title="固定在顶部"></mt-header>
     <!-- 底部区域 -->
     <nav class="mui-bar mui-bar-tab">
-			<a class="mui-tab-item mui-active" href="#tabbar">
+			<router-link class="mui-tab-item" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-chat">
+			</router-link>
+			<router-link class="mui-tab-item" to="/member">
 				<span class="mui-icon mui-icon-contact"></span>
 				<span class="mui-tab-label">会员</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-contact">
+			</router-link>
+			<router-link class="mui-tab-item" to="/shopcar">
 				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
-				<span class="mui-tab-label">通讯录</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-tab-label">购物车</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/search">
 				<span class="mui-icon mui-icon-search"></span>
 				<span class="mui-tab-label">搜索</span>
-			</a>
+			</router-link>
 		</nav>
     <!-- 中间内容区域 -->
+    <router-view></router-view>
   </div>
 </template>
 
@@ -34,6 +35,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less">
+  .app-container {
+    padding-top: 40px;
+    padding-bottom: 50px;
+  }
 </style>
---
+++ src/pages/member/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="member-container">
+        <h1>member</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
+
+
