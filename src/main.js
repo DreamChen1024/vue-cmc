1358e240e49177a3461762f34a29a8a2013ed048
McChen <McChen1024@163.com>
2018-11-19 21:21:56 +0800

    完成轮播图

--- src/main.js
+++ src/main.js
@@ -3,14 +3,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router/'
 
+//导入vue-resource 与后台交互
+import VueResource from 'vue-resource'
+// 注册
+Vue.use(VueResource)
+
 Vue.config.productionTip = false
 
 // 按需导入需要的mint-ui组件
-import { Header } from 'mint-ui'
+import { Header, Swipe, SwipeItem } from 'mint-ui'
 //引入mint-ui的样式(疑似样式包有问题)
 import 'mint-ui/lib/style.css'
 // 注册组件
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 //导入mui的样式
 import './lib/mui/css/mui.css'
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,17 +1,58 @@
 <template>
     <div class="home-container">
-        <h1>home</h1>
+        <mt-swipe class="banner-contarner" :auto="4000">
+            <mt-swipe-item v-for="(item, index) in bannerList" :key="index">
+                <a :href="item.url">
+                    <img :src="item.img" alt="">
+                </a>
+            </mt-swipe-item>
+            
+        </mt-swipe>
     </div>
 </template>
 
 <script>
+import { Toast } from 'mint-ui'
 
 export default {
-    
-}
+    data () {
+        return {
+            bannerList: []
+        }
+    },
+    created() {
+        this.getBannerData()
+    },
+    methods: {
+        getBannerData() {
+            this.$http.get('http://www.lovegf.cn:8899/api/getlunbo').then( result => {
+                if(result.body.status === 0) {
+                    this.bannerList = result.body.message
+                } else {
+                    Toast('轮播图加载失败,请重试!')
+                }
+            })
+        }
+    }
+
+};
 </script>
 
-<style>
+<style lang="less">
+    .home-container {
+        .banner-contarner {
+            width: 100%;
+            height: 200px;
+            a {
+                display: block;
+                width: 100%;
+                height: 100%;
+                img {
+                    width: 100%;
+                }
+            }
+        }
+    }
 
 </style>
 
