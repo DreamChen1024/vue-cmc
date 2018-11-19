807f7d8c0ac5ebe635ad0ec6e9c5692e10481fb1
McChen <McChen1024@163.com>
2018-11-19 19:38:33 +0800

    完成了头部和底部导航并修改了tabBar的图标

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <template>
-  <div id="app">
+  <div class="app-container">
+    <!-- 头部区域 -->
+    <mt-header fixed title="固定在顶部"></mt-header>
+    <!-- 底部区域 -->
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">会员</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-search"></span>
+				<span class="mui-tab-label">搜索</span>
+			</a>
+		</nav>
+    <!-- 中间内容区域 -->
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
+// 入口文件
 import Vue from 'vue'
 import App from './App'
+import router from './router/'
 
 Vue.config.productionTip = false
 
+// 按需导入需要的mint-ui组件
+import { Header } from 'mint-ui'
+//引入mint-ui的样式(疑似样式包有问题)
+import 'mint-ui/lib/style.css'
+// 注册组件
+Vue.component(Header.name, Header)
+
+//导入mui的样式
+import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
